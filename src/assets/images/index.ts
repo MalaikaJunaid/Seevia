a09961796acea59e5b81cdb141f723efa557b55c
mmalaika.junaid <mmalaika.junaid@gmail.com>
2026-03-07 22:38:27 +0500

    Create index.ts

---
+++ src/assets/images/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Seevia Image Registry
+ * Centralized imports for all visual assets.
+ */
+
+export const BRAND_IMAGES = {
+  LOGO_MAIN: require('./SEEVIA-logo.png'),
+  LOGO_DARK: require('./Seevia-Logo-dark.jpg'),
+  LOGO_LIGHT: require('./Seevia-Logo-light.png'),
+  APP_ICON: require('./icon.jpg'),
+};
