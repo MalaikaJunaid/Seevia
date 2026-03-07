0f47d63377e350e12a770340411f39653d85ed12
mmalaika.junaid <mmalaika.junaid@gmail.com>
2026-03-07 23:19:30 +0500

    Create index.ts

---
+++ src/assets/data/index.ts
@@ -0,0 +1,6 @@
+export const LOCAL_DATA = {
+  STORE_MAP: require('./save_mart_pwd_map.json'),
+  ALLERGY_DB: require('./allergy_database.json'),
+  CATEGORIES: require('./pantry_categories.json'),
+  VOICE_PHRASES: require('./emergency_phrases.json'),
+};
