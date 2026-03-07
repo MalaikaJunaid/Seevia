0c25f991339e8526556bc88f5bc70d3c0b84c26d
mmalaika.junaid <mmalaika.junaid@gmail.com>
2026-03-07 19:22:08 +0500

    Create index.ts

---
+++ src/models/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Seevia Models Barrel Export
+ * Centralizes all data interfaces and enums for the Seevia ecosystem.
+ * Use this to import multiple models: import { UserProfile, PantryItem } from '@/src/models';
+ */
+
+export * from './PantryItem';
+export * from './Product';
+export * from './User';
+export * from './VoiceCommand';
